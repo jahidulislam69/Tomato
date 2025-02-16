151db1bdce969889876133126eae6ad4c4cd8bd0
jahidulislam69 <jahidulhassa777@gmail.com>
2025-02-17 00:14:41 +0600

    change track order

--- frontend/src/Pages/MyOrders/MyOrders.jsx
+++ frontend/src/Pages/MyOrders/MyOrders.jsx
@@ -42,7 +42,7 @@ const MyOrders = () => {
                             <p>${order.amount}.00</p>
                             <p>Items: {order.items.length}</p>
                             <p><span>#fgs54g;</span> <b>{order.status}</b></p>
-                            <button>Track Order</button>
+                            <button onClick={fetchOrders}>Track Order</button>
 
                         </div>
                     )
