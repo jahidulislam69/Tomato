009a199cc2f849899ddb4109dba1bdc1066001f7
jahidulislam69 <jahidulhassa777@gmail.com>
2025-02-16 10:01:45 +0600

    Removed Frontend submodule

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,8 +4,8 @@ import Sidebar from './components/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
+import Orders from './pages/Orders/Orders'
 
 
 const App = () => {
